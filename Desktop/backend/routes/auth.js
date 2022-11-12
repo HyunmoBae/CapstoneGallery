7d89654b64a57e6fa53dfc357b15754ff5437ac2
HyunmoBae <tsi0520@cu.ac.kr>
2022-11-12 17:39:55 +0900

    login

---
+++ Desktop/backend/routes/auth.js
@@ -0,0 +1,53 @@
+import { Router } from "express";
+import { connect } from "mongoose";
+import { User } from "../model/user.js";
+import pkg from "bcryptjs";
+import { MONGO_URL } from "../setting.js";
+const { genSalt, hash, compare } = pkg;
+
+const authRouter = Router();
+// const User = mongoose.model("user", UserSchema);
+
+authRouter.post("/register", async (req, res) => {
+  try {
+    await connect(MONGO_URL);
+    const salt = await genSalt(10);
+    const hashPassword = await hash(req.body.password, salt);
+    const newUser = new User({
+      id: req.body.id,
+      password: hashPassword,
+      email: req.body.email,
+      major: req.body.major,
+      tel: req.body.tel,
+      //      password: hashPassword,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    await connect(MONGO_URL);
+    const user = await User.findOne({ id: req.body.id });
+    if (!user) {
+      res.status(404).json("not found");
+    } else {
+      const vaildated = await compare(req.body.password, user.password);
+
+      if (!vaildated) {
+        res.status(400).json("wrong password");
+      } else {
+        res.status(200).json("login success");
+      }
+    }
+  } catch (error) {
+    res.status(500).json(err);
+    console.log(error);
+  }
+});
+
+export { authRouter };
